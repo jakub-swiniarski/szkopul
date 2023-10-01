72cff1de8a8ee1f046d18c7a09106da3244519e7
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-10-01 18:25:17 +0200

    updated solution

--- bankiet.cpp
+++ bankiet.cpp
@@ -8,19 +8,18 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    const unsigned short MAX=30000;
-
-    unsigned short n;
+    short n; //for some reason when i'm using unsigned short, it doesn't give me the right answer
     cin>>n;
-    unsigned short guests[MAX];
-    bool checked[MAX]; 
-    for(unsigned short i=1; i<=n; i++){ //zaczynamy od 1, bo numeracja sasiadow w wejsciu zaczyna sie od 1
+    unsigned short guests[n];
+    bool checked[n]; 
+    for(unsigned short i=0; i<n; i++){
         cin>>guests[i];
+        guests[i]--; //pomniejszamy wartosc, zeby numeracja gosci zaczynala sie od zera, tak samo jak numeracja indeksow tablicy
     }
 
     unsigned short currentIndex; 
     unsigned short tableCount=0;
-    for(unsigned short i=1; i<=n; i++){
+    for(unsigned short i=0; i<n; i++){
         if(!checked[i]){
             currentIndex=i;
             while(!checked[currentIndex]){
