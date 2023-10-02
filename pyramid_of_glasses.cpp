021316e173690de9ee4073323e1be5f8733fc311
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-10-02 18:11:52 +0200

    pyramid of glasses (codeforces)

---
+++ pyramid_of_glasses.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/contest/676/problem/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int height, time;
+    const int capacity=1024; //every glass has a capacity of 1024 to avoid floating point numbers
+    cin>>height>>time;
+    int pyramid[height][height];
+    int fullGlasses=0;
+    pyramid[0][0]=time*capacity;
+    
+    for(int i=0; i<height; i++){
+        for(int j=0; j<=i; j++){
+            if(pyramid[i][j]>=capacity){
+                fullGlasses++;
+
+                int excess=pyramid[i][j]-capacity;
+                pyramid[i+1][j]=excess/2;
+                pyramid[i+1][j+1]=excess/2;
+            }
+        }
+    }
+    cout<<fullGlasses<<'\n';
+
+    return 0;
+}
