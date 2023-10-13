36464ca199b89f559c4308259d734c3c6800fdb4
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-10-13 19:38:05 +0200

    faster solution

--- agar_io.cpp
+++ agar_io.cpp
@@ -20,34 +20,36 @@ int main(){
     sort(enemies.begin(), enemies.end());
 
     int time=0;
-    long long biggest=0, biggestIndex=0;
+    int right=0, middle=0; //for binary search, in this case left is not needed
     while(player<enemies.back()){
-        //check if it's possible to win
+        //check if it's possible to win 
         if(player<=enemies[0]){
-            cout<<"NIE";
+            cout<<"NIE\n";
             return 0;
         }
 
         //look for the biggest ball we can eat
-        for(int i=0; i<enemies.size(); i++){
-            if(enemies[i]<player){
-                biggest=enemies[i];
-                biggestIndex=i;
+        right=enemies.size()-1;
+        middle=right/2;
+        for(;;){
+            if(enemies[middle]>=player){
+                right=middle;
+                middle=right/2;
             }
             else{
+                //eat the found ball
+                player+=enemies[middle];
+                time++;
+                enemies.erase(enemies.begin()+middle);
                 break;
             }
         }
-        //eat the found ball
-        player+=biggest;
-        time++;
-        enemies.erase(enemies.begin()+biggestIndex);
-    
-        if(player>=enemies.back()){
-            cout<<time;
+
+        if(player>=enemies.back() || enemies.size()<1){
+            cout<<time<<'\n';
             return 0;
         }
-    }
+    } 
 
     return 0;
 }
