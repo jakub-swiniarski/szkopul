fd3470688924fdb7f8d3b1947fd31fde99f9b4e0
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-12-20 21:28:48 +0100

    Update bankiet.cpp

--- bankiet.cpp
+++ bankiet.cpp
@@ -8,18 +8,18 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    short n;
+    int n;
     cin>>n;
-    unsigned short guests[n];
+    int guests[n];
     bool checked[n]; 
-    for(unsigned short i=0; i<n; i++){
+    for(int i=0; i<n; i++){
         cin>>guests[i];
         guests[i]--; //pomniejszamy wartosc, zeby numeracja gosci zaczynala sie od zera, tak samo jak numeracja indeksow tablicy
     }
 
-    unsigned short currentIndex; 
-    unsigned short tableCount=0;
-    for(unsigned short i=0; i<n; i++){
+    int currentIndex; 
+    int tableCount=0;
+    for(int i=0; i<n; i++){
         if(!checked[i]){
             currentIndex=i;
             while(!checked[currentIndex]){
