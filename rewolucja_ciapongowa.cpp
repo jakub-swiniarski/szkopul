2a1b544bb0b0d2697be0547d8a019f80da9ff44f
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-24 19:38:08 +0200

    improvements

--- rewolucja_ciapongowa.cpp
+++ rewolucja_ciapongowa.cpp
@@ -10,20 +10,20 @@ int main(void) {
 
     int ile_miast, ile_pytan;
     cin >> ile_miast >> ile_pytan;
-    long long czas_przejazdu[ile_miast - 1];
-    for (int i = 0; i < ile_miast - 1; i++)
+
+    long long czas_przejazdu[ile_miast], sum_pref[ile_miast];
+    czas_przejazdu[0] = sum_pref[0] = 0;
+    for (int i = 1; i < ile_miast; i++) {
         cin >> czas_przejazdu[i];
+        sum_pref[i] = sum_pref[i - 1] + czas_przejazdu[i];
+    }
+
     pair<int, int> pytania[ile_pytan];
     for (int i = 0; i < ile_pytan; i++) {
         cin >> pytania[i].first >> pytania[i].second;
         pytania[i].first--;
         pytania[i].second--;
     }
-
-    long long sum_pref[ile_miast];
-    sum_pref[0] = 0;
-    for (int i = 1; i < ile_miast; i++)
-        sum_pref[i] = sum_pref[i - 1] + czas_przejazdu[i - 1];
 
     for (int i = 0; i < ile_pytan; i++) {
         if (pytania[i].first > pytania[i].second)
