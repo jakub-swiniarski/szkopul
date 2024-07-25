ca5679a4ae856f3fad4092b133eed4344e76ac79
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-25 23:16:51 +0200

    add comments

--- dziwne_czasteczki.cpp
+++ dziwne_czasteczki.cpp
@@ -25,16 +25,17 @@ int main(void) {
     suma_ladunkow[0] = czasteczki[0];
 
     ll wynik = 0;
-    ll ile_max[static_cast<ll>(1e6 + 1)] = { 0 }; // ile jest czasteczek o maksymalnej wartosci i
+    ll ile_max[static_cast<ll>(1e6 + 1)] = { 0 }; // ile jest czasteczek o maksymalnym ladunku i (nie liczac aktualnie rozpatrywanej czasteczki)
     for (ll i = 1; i < ile_czasteczek; i++) {
         suma_ladunkow[i] = suma_ladunkow[i - 1] + czasteczki[i];
 
         for (ll j = czasteczki[i - 1] + 1; j <= czasteczki[i]; j++)
             ile_max[j] = ile_max[czasteczki[i - 1]];
 
-        ll ile_bliskich = ile_max[czasteczki[i]] - ile_max[max(0ll, czasteczki[i] - min_oddzialywanie - 1)];
-        wynik += ile_bliskich * min_oddzialywanie;
-        wynik += czasteczki[i] * (i - 1 - ile_bliskich) - suma_ladunkow[i - 1 - ile_bliskich];
+        ll ile_bliskich = ile_max[czasteczki[i]] - ile_max[max(0ll, czasteczki[i] - min_oddzialywanie - 1)]; // ile jest czasteczek oddzialywujacych minimalna wartoscia, uzywamy max(), aby nie wyjsc poza tablice
+        ll ile_dalekich = i - 1 - ile_bliskich; // ile jest czasteczek oddzialywujacych wartoscia wieksza od minimalnej
+        wynik += ile_bliskich * min_oddzialywanie; // dla czasteczek oddzialywujacych minimalna wartoscia
+        wynik += czasteczki[i] * ile_dalekich - suma_ladunkow[ile_dalekich]; // dla reszty czasteczek
 
         ile_max[czasteczki[i]]++;
     }
