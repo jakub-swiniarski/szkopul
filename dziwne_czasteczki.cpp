69461bd5793b8c54e7a3162801934d6d009e0a80
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-25 22:52:16 +0200

    use long longs

--- dziwne_czasteczki.cpp
+++ dziwne_czasteczki.cpp
@@ -4,33 +4,35 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int main(void) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    long long ile_czasteczek, min_oddzialywanie;
+    ll ile_czasteczek, min_oddzialywanie;
     cin >> ile_czasteczek >> min_oddzialywanie;
     ile_czasteczek++; // dodajemy czasteczke o ladunku 0, nie zmieni wyniku, ale bedzie latwiej z sumami prefiksowymi
     
-    long long czasteczki[ile_czasteczek];
+    ll czasteczki[ile_czasteczek];
     czasteczki[0] = 0;
-    for (long long i = 1; i < ile_czasteczek; i++)
+    for (ll i = 1; i < ile_czasteczek; i++)
         cin >> czasteczki[i];
 
     sort(czasteczki, czasteczki + ile_czasteczek);
 
-    long long suma_ladunkow[ile_czasteczek]; // suma prefiksowa ladunkow
+    ll suma_ladunkow[ile_czasteczek]; // suma prefiksowa ladunkow
     suma_ladunkow[0] = czasteczki[0];
 
-    long long wynik = 0;
-    long long ile_max[static_cast<long long>(1e6 + 1)] = { 0 }; // ile jest czasteczek o maksymalnej wartosci i
-    for (long long i = 1; i < ile_czasteczek; i++) {
+    ll wynik = 0;
+    ll ile_max[static_cast<ll>(1e6 + 1)] = { 0 }; // ile jest czasteczek o maksymalnej wartosci i
+    for (ll i = 1; i < ile_czasteczek; i++) {
         suma_ladunkow[i] = suma_ladunkow[i - 1] + czasteczki[i];
 
-        for (long long j = czasteczki[i - 1] + 1; j <= czasteczki[i]; j++)
+        for (ll j = czasteczki[i - 1] + 1; j <= czasteczki[i]; j++)
             ile_max[j] = ile_max[czasteczki[i - 1]];
 
-        long long ile_bliskich = ile_max[czasteczki[i]] - ile_max[max(0ll, czasteczki[i] - min_oddzialywanie - 1)];
+        ll ile_bliskich = ile_max[czasteczki[i]] - ile_max[max(0ll, czasteczki[i] - min_oddzialywanie - 1)];
         wynik += ile_bliskich * min_oddzialywanie;
         wynik += czasteczki[i] * (i - 1 - ile_bliskich) - suma_ladunkow[i - 1 - ile_bliskich];
 
