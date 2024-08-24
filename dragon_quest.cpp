b7227839b186a6f261b3f8c9a4e9f65990bc70f1
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-08-24 13:35:23 +0200

    comment

--- dragon_quest.cpp
+++ dragon_quest.cpp
@@ -16,7 +16,7 @@ int main(void) {
         int hp, n, m;
         cin >> hp >> n >> m;
 
-        while (n > 0 && hp > 20) {
+        while (n > 0 && hp > 20) { // jesli hp < 20, to pierwsze zaklecie zwiekszy hp
             n--;
             hp = (hp / 2) + 10;
         }
